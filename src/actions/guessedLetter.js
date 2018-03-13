cf1dada103505201e5d408f41bbfa777562e8d80
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-03-13 16:27:25 +0100

    Exported constant GUESSED_LETTER in actions/types

--- src/actions/guessedLetter.js
+++ src/actions/guessedLetter.js
@@ -1,6 +1,8 @@
+import { GUESSED_LETTER } from './types'
+
 export const guessedLetter = function(input) {
   return {
-    type: 'GUESSED_LETTER',
+    type: GUESSED_LETTER,
     payload: {
       guesses: input
     }
--- src/reducers/guesses.js
+++ src/reducers/guesses.js
@@ -1,20 +1,10 @@
-// src/reducers/board.js
-//
-// The default export for a reducer is the reducer function, that takes 2
-// arguments:
-//
-//   - the (current) state, should default to the initial state
-//   - an action Object:
-//     - type : the action type
-//     - payload : [optional] payload
-//
 
-import { guessedLetter } from '../actions/guessedLetter';
+import { GUESSED_LETTER } from '../actions/types';
 import { CREATE_GAME } from '../actions/types'
 
 export default (state = [], { type, payload } = {}) => {
   switch (type) {
-    case 'GUESSED_LETTER':
+    case GUESSED_LETTER:
       return state.concat(payload.guesses)
     case CREATE_GAME:
       return []
